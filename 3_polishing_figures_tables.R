f05acb580968fbe1ede210d5b2bd4b7144625874
celenakim <celenakim2026@u.northwestern.edu>
2023-11-22 20:29:29 -0500

    saved first table png

--- 3_polishing_figures_tables.R
+++ 3_polishing_figures_tables.R
@@ -1,14 +1,19 @@
 library(tidyverse)
-
+library(knitr)
 
 ## Exploration 1: What is the most popular/successful genre?
-unique_genres <- unique(movie_data$genre)
-
 genre_opening_success <- movie_data |> 
   group_by(genre) |> 
   summarize(mean_opening_success = mean(opening_weekend)) |> 
-  arrange(desc(mean_opening_success))
+  arrange(desc(mean_opening_success)) |> 
+  slice_head(n = 3) |> 
+  DT::datatable()
 genre_opening_success
+# ![]("figures"/"genre_opening_success_mean.png")
+
+
+
+
 
 genre_oscar_counts <- movie_data |> 
   group_by(genre) |> 
