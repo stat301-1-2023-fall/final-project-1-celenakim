4fd3010eb82a39d9c79d2198319f7ad17452635e
celenakim <celenakim2026@u.northwestern.edu>
2023-11-22 19:31:03 -0500

    got rid of code

--- 0b_data_cleaning.R
+++ 0b_data_cleaning.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(janitor)
 
 
+## JOINING DATA SETS
 ## Deleting last column from 2011-2018 data frames
 years_one <- 2011:2018
 for (year in years_one) {
@@ -26,8 +27,6 @@ for (year in years_two) {
   assign(paste0("data_", year), current_data)
 }
 
-
-
 movie_data <- rbind(data_2007, data_2008, data_2009,
                     data_2010, data_2011, data_2012,
                     data_2013, data_2014, data_2015,
@@ -35,11 +34,52 @@ movie_data <- rbind(data_2007, data_2008, data_2009,
                     data_2019, data_2020, data_2021,
                     data_2022)
 
-write.csv(movie_data, "movie_data.csv", row.names = FALSE)
+write.csv(movie_data, "data/movie_data.csv", row.names = FALSE)
+
+## CLEANING DATA
+# Changing variable names to snake case
+movie_data <- clean_names(movie_data)
+
+# Deleting unnecessary variables
+drop = c("link", "none")
+movie_data = movie_data[,!(names(movie_data) %in% drop)]
+
+# Changing character variables to integers when needed
+char_vector <- c("rotten_tomatoes_critics", "metacritic_critics", 
+                 "average_critics", "metacritic_audience",
+                 "rotten_tomatoes_vs_metacritic_deviance", 
+                 "average_audience", "audience_vs_critics_deviance", 
+                 "opening_weekend", "opening_weekend_million", 
+                 "domestic_gross", "domestic_gross_million", 
+                 "foreign_gross_million", "foreign_gross", 
+                 "worldwide_gross", "worldwide_gross_million", 
+                 "budget_million")
+
+# Convert the character vector to integer
+movie_data <- movie_data %>%
+  mutate_at(char_vector, ~as.integer(.))
+
+# Replace NA values in 'genre' with values from 'primary_genre'
+movie_data <- movie_data %>%
+  mutate(genre = ifelse(is.na(genre) | 
+                          genre == "-",
+                        primary_genre, 
+                        genre))
+
+# Changing "oscar_winners" variable to boolean
+movie_data <- movie_data %>%
+  mutate(oscar_winners = ifelse(oscar_winners == "", 
+                                FALSE, 
+                                TRUE))
 
 
 
-## Initial ode without for loops
+
+
+
+
+
+## Initial code without for loops
 # data_2011 <- data_2011[, -ncol(data_2011)]
 # 
 # data_2012 <- data_2012[, -ncol(data_2012)]
@@ -81,3 +121,4 @@ write.csv(movie_data, "movie_data.csv", row.names = FALSE)
 #                     data_2016, data_2017, data_2018, 
 #                     data_2019, data_2020, data_2021, 
 #                     data_2022)
+
